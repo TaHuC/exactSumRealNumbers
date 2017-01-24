1eaf8b293405d31a4d195625dec699c499ac0900
TaHuC <TaHuC@hotmail.com>
2017-01-24 21:05:03 +0200

    Test console upload on github

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exactSumRealNumbers
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			decimal finalSum = 0;
+
+			for (int i = 0; i < n; i++)
+			{
+				decimal sumNumber = decimal.Parse(Console.ReadLine());
+				finalSum += sumNumber;
+			}
+
+			Console.WriteLine(finalSum);
+		}
+	}
+}
